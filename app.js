52b534b0818c89bfa777eb5d3300984885a797f8
Sumit Kumar <bingostudypoint@gmail.com>
2024-08-26 22:37:24 +0530

    bot

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('login', (username) => {
+    socket.username = username
+    io.emit('user-connected', username)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+    io.emit('user-disconnected', socket.username)
+  })
+
+  socket.on('message', (data) => {
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback', (data) => {
+    socket.broadcast.emit('feedback', data)
+  })
+}
